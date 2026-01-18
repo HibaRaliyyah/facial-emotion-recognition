41dde18f7568686c7fd84a1c46bb6c83fe17466b
HibaRaliyyah <hibaraliyyah12@gmail.com>
2026-01-18 07:46:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
 import cv2
@@ -10,6 +11,7 @@ import mediapipe as mp
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_DIR = os.path.join(BASE_DIR, "models")
